9280d6db678306b89a6dd033a071da334a56306b
Nithesh Raaj <nithraaj@gmail.com>
2018-03-26 15:42:34 +0530

    added complete login logic

--- app/src/main/java/karan/cogz/LoginPage.java
+++ app/src/main/java/karan/cogz/LoginPage.java
@@ -25,6 +25,7 @@ public class LoginPage extends AppCompatActivity {
     EditText username, password;
     Button login;
     TextView register;
+    TextView err_msg;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     Context context;
@@ -33,21 +34,25 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
         context = this;
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         editor = sharedPreferences.edit();
+
         AndroidNetworking.initialize(getApplicationContext());
+
         username = (EditText) findViewById(R.id.editText_username);
         password = (EditText) findViewById(R.id.editText_password);
         login = (Button) findViewById(R.id.button_login);
-        //adminlogin = (Button) findViewById(R.id.button_loginadmin);
         register = (TextView) findViewById(R.id.text_register);
-        // login();
-        signup();
+        err_msg = findViewById(R.id.error_msg);
+
+        setViewOnclickListeners();
     }
 
     public void login() {
         try {
+            err_msg.setVisibility(View.GONE);
             JSONObject jsonObject = new JSONObject();
             JSONObject data = new JSONObject();
             data.put("username", username.getText().toString());
@@ -67,7 +72,8 @@ public class LoginPage extends AppCompatActivity {
                                 editor.putString("username", response.getString("username"));
                                 editor.putInt("hasura_id", response.getInt("hasura_id"));
                                 editor.putString("acc_type", "student");
-                                // todo: redirect to chat screen
+                                Intent intent = new Intent(context,HomePage.class);
+                                context.startActivity(intent);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -77,9 +83,27 @@ public class LoginPage extends AppCompatActivity {
                         public void onError(ANError error) {
                             int errCode = error.getErrorCode();
                             if (errCode == 400) {
+                                try {
+                                    JSONObject errorbody = new JSONObject(error.getErrorBody());
+                                    if(errorbody.getString("message").equals("Invalid credentials")){
+                                        err_msg.setText("Incorrect credentials");
+                                        err_msg.setVisibility(View.VISIBLE);
+                                    }
+                                    else{
+                                        err_msg.setText("Unable to login, Please try again");
+                                        err_msg.setVisibility(View.VISIBLE);
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
                                 //most likely username already exist, check error body to confirm
                             }
-
+                            else{
+                                err_msg.setText("Unable to login, Please try again");
+                                err_msg.setVisibility(View.VISIBLE);
+                            }
                             // handle error
                         }
                     });
@@ -88,17 +112,23 @@ public class LoginPage extends AppCompatActivity {
         }
     }
 
-    public void signup() {
+    public void setViewOnclickListeners() {
         try {
             register.setOnClickListener(
                     new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            Intent i = new Intent("karan.cogz.SignUp");
+                            Intent i = new Intent(context,SignUp.class);
                             startActivity(i);
                         }
                     }
             );
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    login();
+                }
+            });
         }
         catch(Exception e)
         {
