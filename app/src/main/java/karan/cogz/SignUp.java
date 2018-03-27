39af3a40913cf03b000f6d30cb61d26fdf0680b1
Nithesh Raaj <nithraaj@gmail.com>
2018-03-27 18:14:11 +0530

    bug fixes in signup

--- app/src/main/java/karan/cogz/SignUp.java
+++ app/src/main/java/karan/cogz/SignUp.java
@@ -27,6 +27,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
+
 public class SignUp extends AppCompatActivity {
 
 
@@ -43,8 +45,10 @@ public class SignUp extends AppCompatActivity {
     AutoCompleteTextView textViewDomain;
     TextView errormsg;
     EditText name;
+    EditText passconf;
     ProgressBar pb;
-
+    String[] arrayC = {"SSN", "RMK", "RMD", "Velammal Engineering College", "Sathyabama", "St.Joseph's Institute of Technology", "RMKCET", "SRM University", "Velammal institute of Technology","B.S.Abdur Rahman Institute of Science and Technology", "Sri Venkateswara College of Engineering","Meenakshi Sundararajan College","St.Joseph's College of Engineering","VIT Chennai","IIT Madras"};
+    String[] arrayD = {"HC NA", "LS", "BFS", "BFS-TAO", "AVM CommsMedia", "AVM Ismo", "AVM EAS", "RCGTH", "P&R","MLEU P&R"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +62,7 @@ public class SignUp extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.userName);
         password = (EditText) findViewById(R.id.pass1);
+        passconf = findViewById(R.id.pass2);
         login = findViewById(R.id.text_login);
         signup = (Button) findViewById(R.id.button_sign_up);
         select_mentor = (CheckBox) findViewById(R.id.checkBox);
@@ -72,74 +77,74 @@ public class SignUp extends AppCompatActivity {
 
     public void sign_up() {
         try {
-            errormsg.setVisibility(View.GONE);
-            signup.setVisibility(View.GONE);
-            pb.setIndeterminate(true);
-            pb.setVisibility(View.VISIBLE);
-            // todo: validate data - password length, all fields filled?
-            JSONObject jsonObject = new JSONObject();
-            errormsg.setVisibility(View.GONE);
-            JSONObject data = new JSONObject();
-            data.put("username", username.getText().toString());
-            data.put("password", password.getText().toString());
-            jsonObject.put("provider", "username");
-            jsonObject.put("data", data);
-            AndroidNetworking.post("https://auth." + getString(R.string.cluster_name) + ".hasura-app.io/v1/signup")
-                    .addJSONObjectBody(jsonObject)
-                    .setPriority(Priority.MEDIUM)
-                    .build()
-                    .getAsJSONObject(new JSONObjectRequestListener() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            // do anything with response
-                            try {
-                                editor.putString("token", response.getString("auth_token"));
-                                editor.putString("username", response.getString("username"));
-                                editor.putInt("hasura_id", response.getInt("hasura_id"));
-                                editor.putString("acc_type", "student");
-                                editor.commit();
-                                if (!isMentor) {
-                                    updateOtherData();
-                                } else {
-                                    upgradeAcc();
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onError(ANError error) {
-                            pb.setVisibility(View.GONE);
-                            pb.setIndeterminate(false);
-                            signup.setVisibility(View.VISIBLE);
-                            int errCode = error.getErrorCode();
-                            if (errCode == 400) {
-                                JSONObject errBody = null;
+            Boolean val = validateInput();
+            if(val) {
+                errormsg.setVisibility(View.GONE);
+                signup.setVisibility(View.GONE);
+                pb.setIndeterminate(true);
+                pb.setVisibility(View.VISIBLE);
+                JSONObject jsonObject = new JSONObject();
+                errormsg.setVisibility(View.GONE);
+                JSONObject data = new JSONObject();
+                data.put("username", username.getText().toString());
+                data.put("password", password.getText().toString());
+                jsonObject.put("provider", "username");
+                jsonObject.put("data", data);
+                AndroidNetworking.post("https://auth." + getString(R.string.cluster_name) + ".hasura-app.io/v1/signup")
+                        .addJSONObjectBody(jsonObject)
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // do anything with response
                                 try {
-                                    errBody = new JSONObject(error.getErrorBody());
-                                    if(errBody.getString("message").equals("This user already exists")){
-                                        errormsg.setText("Username already taken");
-                                        errormsg.setVisibility(View.VISIBLE);
-                                    }
-                                    else{
-                                        errormsg.setText("Couldn't sign up, Please try again");
-                                        errormsg.setVisibility(View.VISIBLE);
+                                    editor.putString("token", response.getString("auth_token"));
+                                    editor.putString("username", response.getString("username"));
+                                    editor.putInt("hasura_id", response.getInt("hasura_id"));
+                                    editor.putString("acc_type", "student");
+                                    editor.commit();
+                                    if (!isMentor) {
+                                        updateOtherData();
+                                    } else {
+                                        upgradeAcc();
                                     }
                                 } catch (JSONException e) {
                                     e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError error) {
+                                pb.setVisibility(View.GONE);
+                                pb.setIndeterminate(false);
+                                signup.setVisibility(View.VISIBLE);
+                                int errCode = error.getErrorCode();
+                                if (errCode == 400) {
+                                    JSONObject errBody = null;
+                                    try {
+                                        errBody = new JSONObject(error.getErrorBody());
+                                        if (errBody.getString("message").equals("This user already exists")) {
+                                            errormsg.setText("Username already taken");
+                                            errormsg.setVisibility(View.VISIBLE);
+                                        } else {
+                                            errormsg.setText("Couldn't sign up, Please try again");
+                                            errormsg.setVisibility(View.VISIBLE);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                        errormsg.setText("Couldn't sign up, Please try again");
+                                        errormsg.setVisibility(View.VISIBLE);
+                                    }
+                                } else {
                                     errormsg.setText("Couldn't sign up, Please try again");
                                     errormsg.setVisibility(View.VISIBLE);
                                 }
-                            }
-                            else{
-                                errormsg.setText("Couldn't sign up, Please try again");
-                                errormsg.setVisibility(View.VISIBLE);
-                            }
 
-                            // handle error
-                        }
-                    });
+                                // handle error
+                            }
+                        });
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -178,11 +183,9 @@ public class SignUp extends AppCompatActivity {
                     }
                 }
         );
-        String[] arrayC = {"SSN", "RMK", "RMD", "Velammal Engineering College", "Sathyabama", "St.Joseph's Institute of Technology", "RMKCET", "SRM University", "Velammal institute of Technology","B.S.Abdur Rahman Institute of Science and Technology", "Sri Venkateswara College of Engineering","Meenakshi Sundararajan College","St.Joseph's College of Engineering","VIT Chennai","IIT Madras"};
         ArrayAdapter<String> adapterC = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, arrayC);
         textViewCollege.setAdapter(adapterC);
 
-        String[] arrayD = {"HC NA", "LS", "BFS", "BFS-TAO", "AVM CommsMedia", "AVM Ismo", "AVM EAS", "RCGTH", "P&R","MLEU P&R"};
         ArrayAdapter<String> adapterD = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, arrayD);
         textViewDomain.setAdapter(adapterD);
     }
@@ -199,6 +202,7 @@ public class SignUp extends AppCompatActivity {
                     .getAsJSONObject(new JSONObjectRequestListener() {
                         @Override
                         public void onResponse(JSONObject response) {
+                            Log.d("UPGRADE OK",response.toString());
                             // do anything with response
                             editor.putString("acc_type", "mentor");
                             editor.commit();
@@ -207,7 +211,7 @@ public class SignUp extends AppCompatActivity {
 
                         @Override
                         public void onError(ANError error) {
-
+                            Log.d("UPGRADE FAIL",error.getErrorBody());
                             deleteUser();
                             int errCode = error.getErrorCode();
                             if (errCode == 401) {
@@ -341,7 +345,7 @@ public class SignUp extends AppCompatActivity {
 
     private void deleteUser(){
         try {
-
+            Log.d("DELETE USER", "Deleting user");
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("userid", sharedPreferences.getInt("hasura_id",0));
             AndroidNetworking.post("https://api." + getString(R.string.cluster_name) + ".hasura-app.io/user/delete")
@@ -351,6 +355,7 @@ public class SignUp extends AppCompatActivity {
                     .getAsJSONObject(new JSONObjectRequestListener() {
                         @Override
                         public void onResponse(JSONObject response) {
+                            Log.d("DELETE USER OK", response.toString());
                             pb.setVisibility(View.GONE);
                             pb.setIndeterminate(false);
                             signup.setVisibility(View.VISIBLE);
@@ -358,6 +363,7 @@ public class SignUp extends AppCompatActivity {
 
                         @Override
                         public void onError(ANError error) {
+                            Log.d("DELETE USER FAIL",error.getErrorBody());
                             pb.setVisibility(View.GONE);
                             pb.setIndeterminate(false);
                             signup.setVisibility(View.VISIBLE);
@@ -367,6 +373,51 @@ public class SignUp extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
+    }
+    private Boolean validateInput(){
+        Boolean flag = true;
+        if(name.getText().toString().equals("")){
+            errormsg.setText("Name cannot be empty !");
+            errormsg.setVisibility(View.VISIBLE);
+            flag = false;
+            return flag;
+        }
+        else if(username.getText().toString().equals("")){
+            errormsg.setText("Username cannot be empty !");
+            errormsg.setVisibility(View.VISIBLE);
+            flag = false;
+            return flag;
+        }
+        else if(password.getText().toString().length()<6){
+            errormsg.setText("Password must be atleast 6 characters long");
+            errormsg.setVisibility(View.VISIBLE);
+            flag = false;
+            return  flag;
+        }
+        else if(!passconf.getText().toString().equals(password.getText().toString())){
+            errormsg.setText("Passwords do not match");
+            errormsg.setVisibility(View.VISIBLE);
+            flag = false;
+            return flag;
+        }
+        if(isMentor){
+            String domain = textViewDomain.getText().toString();
+            if(!Arrays.asList(arrayD).contains(domain)){
+                errormsg.setText("Invalid domain");
+                errormsg.setVisibility(View.VISIBLE);
+                flag = false;
+                return flag;
+            }
+        }
+        else{
+            String clg = textViewCollege.getText().toString();
+            if(!Arrays.asList(arrayC).contains(clg)){
+                errormsg.setText("Invalid College");
+                errormsg.setVisibility(View.VISIBLE);
+                flag = false;
+                return flag;
+            }
+        }
+        return flag;
     }
 }
