bd26e44e5b1a948b3d80556305873cd99cb75932
Nithesh Raaj <nithraaj@gmail.com>
2018-03-27 19:47:18 +0530

    fixed crash

--- app/src/main/java/karan/cogz/HomePage.java
+++ app/src/main/java/karan/cogz/HomePage.java
@@ -35,14 +35,6 @@ import org.json.JSONObject;
 
 public class HomePage extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
