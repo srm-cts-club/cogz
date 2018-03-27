20abe16e3f1d8537d3efcfe803edf12c407cdd05
Karan <krmatalia@gmail.com>
2018-03-27 21:44:39 +0530

    about page java

--- app/src/main/java/karan/cogz/AboutPage.java
+++ app/src/main/java/karan/cogz/AboutPage.java
@@ -1,13 +1,34 @@
 package karan.cogz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutPage extends AppCompatActivity {
-
+ Button butt_on;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+        butt_on=(Button)findViewById(R.id.button_about);
+        onclick();
+    }
+    public void onclick()
+    {
+        butt_on.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent email = new Intent(Intent.ACTION_SEND);
+                        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"cts.srm@gmail.com"});
+                        email.putExtra(Intent.EXTRA_SUBJECT, "subject");
+                        email.putExtra(Intent.EXTRA_TEXT, "message");
+                        email.setType("message/rfc822");
+                        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                    }
+                }
+        );
     }
 }
